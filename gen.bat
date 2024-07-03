ca391e9f934119f188a181703c859732a1da05f6
Dwisler <tymah4er@gmail.com>
2024-07-03 16:53:50 +0300

    add csv sales examples in data\, add img"

--- gen.bat
+++ gen.bat
@@ -1,3 +1,2 @@
 cd C:\Users\Dmitriy_Ch\Desktop\git\module-8-project
 C:\Users\Dmitriy_Ch\Desktop\git\module-8-project\m8_venv\Scripts\python.exe     C:\Users\Dmitriy_Ch\Desktop\git\module-8-project\generate_retrieval.py
-pause
